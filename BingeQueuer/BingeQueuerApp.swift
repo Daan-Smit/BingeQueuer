08371cc02f889b884c4a3d9d647309f4f07c928a
Daan Smit <daan.smit@kpnmail.nl>
2026-02-05 14:56:40 +0100

    Initial Commit

---
+++ BingeQueuer/BingeQueuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingeQueuerApp.swift
+//  BingeQueuer
+//
+//  Created by DaanSmit on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BingeQueuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
