2fb63f5b1225169f9ca5d124cd80b29044b7feb9
Daan Smit <daan.smit@kpnmail.nl>
2026-02-09 20:23:11 +0100

    finished

---
+++ BingeQueuer/View/Components/BingeRow.swift
@@ -0,0 +1,18 @@
+//
+//  BingeRow.swift
+//  BingeQueuer
+//
+//  Created by DaanSmit on 06/02/2026.
+//
+
+import SwiftUI
+
+struct BingeRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BingeRow()
+}
---
+++ BingeQueuer/View/SeriesDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SeriesDetail.swift
+//  BingeQueuer
+//
+//  Created by DaanSmit on 06/02/2026.
+//
+
+import SwiftUI
+
+struct SeriesDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeriesDetail()
+}
